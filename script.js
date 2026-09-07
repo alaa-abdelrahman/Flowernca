04195283fdfa4dfd06533a40dd52d6b4e784b5c5
Alaa Abdelrahman <143619699+alaa-abdelrahman@users.noreply.github.com>
2026-09-07 14:48:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        pinkPure: {
+          100: "#FFF2F4",
+          200: "#FFE3E7",
+          300: "#FCAEBB",
+          400: "#F3B2BC",
+          500: "#E08594",
+          900: "#3D1C22",
+        },
+        // roseGold: "#D4AF37",
+      },
+      fontFamily: {
+        serif: ["Playfair Display", "Georgia", "serif"],
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+  },
+};
